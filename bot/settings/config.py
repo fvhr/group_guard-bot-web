6195660a95855a57b058241d021cc802ad4bb292
Roma <romantkacenko82@gmail.com>
2024-11-29 21:37:23 +0300

    start_bot

---
+++ bot/settings/config.py
@@ -0,0 +1,13 @@
+
+from dataclasses import dataclass, field
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+
+@dataclass
+class Settings:
+    BOT_TOKEN: str = field(default_factory=lambda: env("BOT_TOKEN"))
+    ADMIN_TG_UUID: int = field(default_factory=lambda: env("ADMIN_TG_UUID"))
---
+++ bot/utils/notify_admins.py
@@ -0,0 +1,7 @@
+from aiogram import Dispatcher
+
+from utils import settings
+
+
+async def on_startup_notify(dp: Dispatcher):
+    await dp.bot.send_message(chat_id=settings.ADMIN_TG_UUID, text='Bot Started')
